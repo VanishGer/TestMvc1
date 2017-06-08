8ad992b2c158fd7fd549003ee1d75b5611ca04d5
Vanish <Vanish@VANISH-NOTEBOOK>
2017-06-09 00:35:53 +0700

    Добавьте файлы проекта.

---
+++ TestMvc/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMvc.Models;
+using TestMvc.Util;
+
+namespace TestMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            IEnumerable<Book> books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return "Спасибо, " + purchase.Person + ", за покупку!";
+        }
+
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2>Привет, мир !</h2>");
+        }
+
+        public ActionResult GetImage()
+        {
+            string path = "../Images/visualstudio.jpg";
+            return new ImageResult(path);
+        }
+
+        public string Info()
+        {
+            string browser = HttpContext.Request.Browser.Browser;
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+            "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
+        }
+
+        public ActionResult Partial()
+        {
+            ViewBag.Message = "Это частичное представление.";
+            return PartialView();
+        }
+    }
+}
---
+++ TestMvc/Controllers/MyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TestMvc.Controllers
+{
+    public class MyController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            string ip = requestContext.HttpContext.Request.UserHostAddress;
+            var responce = requestContext.HttpContext.Response;
+            responce.Write("<h2>Ваш IP-адрес: " + ip + "</h2>");
+        }
+    }
+}
